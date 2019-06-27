02d7099a635bf338c3540f2167ee3c781dfcc742
Unknown <garfieldfan3993@gmail.com>
2019-06-27 21:14:13 +0100

    Fix "that were me" removing wrong messages

--- commands/thatwereme.js
+++ commands/thatwereme.js
@@ -6,11 +6,11 @@ const date = require('date-and-time');
 // exports
 module.exports = {
     async execute(message) {
-            const shortname = message.member.displayName.split(' ').join('').substring(0,8);
+            const shortname = message.member.displayName.split(' ').join('').substring(0, 8);
             const { dateFin } = formatDate();
             Canvas.registerFont('./fonts/trebucbd.ttf', { family: 'Trebuchet MS', weight: 'bold' });
-            Canvas.registerFont('./fonts/Verdana.ttf', { family: 'Verdana'})
-            const canvas = Canvas.createCanvas(242, 70)
+            Canvas.registerFont('./fonts/Verdana.ttf', { family: 'Verdana' });
+            const canvas = Canvas.createCanvas(242, 70);
             const ctx = canvas.getContext('2d');
             const background = await Canvas.loadImage('./images/godiwishbackground.png');
             ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
@@ -24,7 +24,7 @@ module.exports = {
             ctx.fillText(`${shortname}21`, 86, 32);
             ctx.font = '11px "Verdana", sans-serif';
             ctx.fillStyle = '#778584';
-            ctx.fillText(dateFin, 86+ 10 + nameLength, 32);
+            ctx.fillText(dateFin, 86 + 10 + nameLength, 32);
             const canvas2 = Canvas.createCanvas(canvas.width * 2, canvas.height * 2);
             const ctx2 = canvas2.getContext('2d');
             ctx2.drawImage(canvas, 0, 0, canvas2.width, canvas2.height);
@@ -34,8 +34,8 @@ module.exports = {
 			catch (error) {
 				console.error(error);
 			}
-         }
-}
+         },
+};
 function formatDate() {
     const now = new Date();
     const dateFin = date.format(now, 'MMM DD, YYYY');
@@ -44,6 +44,6 @@ function formatDate() {
 module.exports.info = {
     name: 'god i wish that were me',
     description: 'For when you want to get into the boots of BigJB21',
-    summon: 'saying "god i wish that was/were me", also via !god'
-}
-module.exports.regexp = '(^!god|^god i wish that (were|was) me)'
+    summon: 'saying "god i wish that was/were me", also via !god',
+};
+module.exports.regexp = '(^!god|^god i wish that (were|was) me($| $))';
